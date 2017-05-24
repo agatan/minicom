b2913196c38df8c8e811f030bb0f56a28c48d186
agatan <agatan039@gmail.com>
2017-05-24 21:30:35 +0900

    Move Expr definition

--- src/ast/mod.rs
+++ src/ast/mod.rs
@@ -1,6 +1,4 @@
-mod expr;
 mod types;
-pub use self::expr::*;
 pub use self::types::*;
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
@@ -14,4 +12,46 @@ impl NodeId {
 
 pub trait Node {
     fn get_id(&self) -> NodeId;
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub enum ExprKind {
+    Int(i64),
+    Float(f64),
+    Add(Box<Expr>, Box<Expr>),
+    Sub(Box<Expr>, Box<Expr>),
+    Mul(Box<Expr>, Box<Expr>),
+    Div(Box<Expr>, Box<Expr>),
+    Parens(Box<Expr>),
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct Expr {
+    pub id: NodeId,
+    pub kind: ExprKind,
+    pub typ: Type,
+}
+
+impl Expr {
+    pub fn new(id: NodeId, kind: ExprKind) -> Self {
+        Expr {
+            id: id,
+            kind: kind,
+            typ: Type::hole(),
+        }
+    }
+
+    pub fn with_typ(id: NodeId, kind: ExprKind, typ: Type) -> Self {
+        Expr {
+            id: id,
+            kind: kind,
+            typ: typ,
+        }
+    }
+}
+
+impl Node for Expr {
+    fn get_id(&self) -> NodeId {
+        self.id
+    }
 }
