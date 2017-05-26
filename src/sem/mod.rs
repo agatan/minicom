6fad29d99a864ca3aa9d7be041f75b4ecc671d21
agatan <agatan039@gmail.com>
2017-05-26 20:06:39 +0900

    Move Checker

--- src/sem/mod.rs
+++ src/sem/mod.rs
@@ -1,9 +1,9 @@
 pub mod ir;
 mod typing;
 
-use self::ir::Type;
-use ast::NodeId;
-pub use self::typing::{TypeMap, Checker};
+use self::ir::{Node, NodeKind, Type};
+use ast::{NodeId, Node as AstNode, NodeKind as AstNodeKind};
+pub use self::typing::TypeMap;
 
 error_chain! {
     types {
@@ -19,6 +19,106 @@ error_chain! {
         CannotInfer(x: NodeId) {
             description("cannot infer type")
             display("cannot infer type: {:?}", x)
+        }
+    }
+}
+
+#[derive(Debug)]
+pub struct Checker {
+    pub typemap: TypeMap,
+}
+
+impl Checker {
+    pub fn check(nodes: &[AstNode]) -> Result<(Vec<Node>, TypeMap)> {
+        let mut chk = Self::new();
+        let mut results = Vec::with_capacity(nodes.len());
+        for node in nodes {
+            chk.transform_node(node).map(|node| results.push(node))?;
+        }
+        Ok((results, chk.typemap))
+    }
+
+    pub fn new() -> Self {
+        Checker { typemap: TypeMap::new() }
+    }
+
+    pub fn transform_node(&mut self, node: &AstNode) -> Result<Node> {
+        let e = self.transform_node_(node)?;
+        self.typemap.insert(node.id, e.typ.clone());
+        Ok(e)
+    }
+
+    fn transform_node_(&mut self, node: &AstNode) -> Result<Node> {
+        match node.kind {
+            AstNodeKind::Int(n) => Ok(Node::new(NodeKind::Int(n), Type::Int)),
+            AstNodeKind::Float(f) => Ok(Node::new(NodeKind::Float(f), Type::Float)),
+            AstNodeKind::Add(ref l, ref r) => {
+                let left = self.transform_node(l)?;
+                let right = self.transform_node(r)?;
+                let lty = self.typemap.get(l.id);
+                let rty = self.typemap.get(r.id);
+                if lty != rty {
+                    return Err(ErrorKind::InvalidTypeUnification(lty, rty).into());
+                }
+                if lty == Type::Int {
+                    Ok(Node::new(NodeKind::AddInt(Box::new(left), Box::new(right)), Type::Int))
+                } else {
+                    Ok(Node::new(NodeKind::AddFloat(Box::new(left), Box::new(right)),
+                                 Type::Float))
+                }
+            }
+            AstNodeKind::Sub(ref l, ref r) => {
+                let left = self.transform_node(l)?;
+                let right = self.transform_node(r)?;
+                let lty = self.typemap.get(l.id);
+                let rty = self.typemap.get(r.id);
+                if lty != rty {
+                    return Err(ErrorKind::InvalidTypeUnification(lty, rty).into());
+                }
+                if lty == Type::Int {
+                    Ok(Node::new(NodeKind::SubInt(Box::new(left), Box::new(right)), Type::Int))
+                } else {
+                    Ok(Node::new(NodeKind::SubFloat(Box::new(left), Box::new(right)),
+                                 Type::Float))
+                }
+            }
+            AstNodeKind::Mul(ref l, ref r) => {
+                let left = self.transform_node(l)?;
+                let right = self.transform_node(r)?;
+                let lty = self.typemap.get(l.id);
+                let rty = self.typemap.get(r.id);
+                if lty != rty {
+                    return Err(ErrorKind::InvalidTypeUnification(lty, rty).into());
+                }
+                if lty == Type::Int {
+                    Ok(Node::new(NodeKind::MulInt(Box::new(left), Box::new(right)), Type::Int))
+                } else {
+                    Ok(Node::new(NodeKind::MulFloat(Box::new(left), Box::new(right)),
+                                 Type::Float))
+                }
+            }
+            AstNodeKind::Div(ref l, ref r) => {
+                let left = self.transform_node(l)?;
+                let right = self.transform_node(r)?;
+                let lty = left.typ.clone();
+                let rty = right.typ.clone();
+                if lty != rty {
+                    return Err(ErrorKind::InvalidTypeUnification(lty, rty).into());
+                }
+                if lty == Type::Int {
+                    Ok(Node::new(NodeKind::DivInt(Box::new(left), Box::new(right)), Type::Int))
+                } else {
+                    Ok(Node::new(NodeKind::DivFloat(Box::new(left), Box::new(right)),
+                                 Type::Float))
+                }
+            }
+            AstNodeKind::Parens(ref e) => self.transform_node(e),
+            AstNodeKind::Print(ref e) => {
+                let e = self.transform_node(e)?;
+                let ty = e.typ.clone();
+                Ok(Node::new(NodeKind::Print(Box::new(e)), ty))
+            }
+            AstNodeKind::Let(_) => unimplemented!(),
         }
     }
 }
