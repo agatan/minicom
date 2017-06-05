81c7ed69c55c84ecf366830db5657bb33187267d
agatan <agatan039@gmail.com>
2017-06-05 22:23:11 +0900

    Remove bytecode interpreter

--- src/main.rs
+++ src/main.rs
@@ -11,8 +11,6 @@ extern crate libc;
 extern crate minivm_syntax as syntax;
 
 mod sem;
-mod bytecode_compiler;
-mod vm;
 mod llvm;
 mod compiler;
 
@@ -22,8 +20,7 @@ use std::fs::File;
 use syntax::ast;
 
 use sem::Context;
-use bytecode_compiler::Compiler;
-use vm::{Value, Machine};
+use compiler::Compiler;
 
 macro_rules! try_or_exit {
     ($x:expr) => {
@@ -41,9 +38,7 @@ fn main() {
     try_or_exit!(env_logger::init());
 
     let mut ctx = Context::new();
-    let mut bcompiler = Compiler::new();
-    let mut compiler = compiler::Compiler::new();
-    let mut machine = Machine::new();
+    let mut compiler = ::Compiler::new();
 
     let contents = match ::std::env::args().nth(1) {
         None => {
@@ -58,19 +53,10 @@ fn main() {
             contents
         }
     };
-    try_or_exit!(run(&mut machine,
-                     &mut bcompiler,
-                     &mut compiler,
-                     &mut ctx,
-                     &contents));
+    try_or_exit!(run(&mut compiler, &mut ctx, &contents));
 }
 
-fn run(machine: &mut Machine,
-       bcompiler: &mut Compiler,
-       compiler: &mut compiler::Compiler,
-       ctx: &mut Context,
-       input: &str)
-       -> Result<Value, String> {
+fn run(compiler: &mut Compiler, ctx: &mut Context, input: &str) -> Result<(), String> {
     let nodes = syntax::parse(input).map_err(|err| format!("{}", err))?;
     debug!("nodes: {:?}", nodes);
     let prog = ctx.transform(nodes).map_err(|err| format!("{}", err))?;
@@ -84,107 +70,5 @@ fn run(machine: &mut Machine,
         }
         Err(err) => println!("{}", err),
     }
-    let instrs = bcompiler.compile(&prog);
-    debug!("instrs: {:?}", instrs);
-    Ok(machine.run(bcompiler.funcs(), &instrs))
-}
-
-#[cfg(test)]
-mod tests {
-    use syntax;
-    use sem::Context;
-    use bytecode_compiler::Compiler;
-    use vm::{Value, Machine};
-
-    fn run(input: &str) -> Result<Value, String> {
-        let mut machine = Machine::new();
-        let mut compiler = Compiler::new();
-        let mut ctx = Context::new();
-        let nodes = syntax::parse(input).map_err(|err| format!("{}", err))?;
-        let prog = ctx.transform(nodes).map_err(|err| format!("{}", err))?;
-        let instrs = compiler.compile(&prog);
-        Ok(machine.run(compiler.funcs(), &instrs))
-    }
-
-    #[test]
-    fn test_primitives() {
-        let tests: &[(&str, Value)] = &[(r#"0"#, Value::Int(0)),
-                                        (r#" 1.0 "#, Value::Float(1.0)),
-                                        (r#"1 + 1"#, Value::Int(2)),
-                                        (r#"1 + 2 * 3 / 4"#, Value::Int(2))];
-
-        for &(input, expected) in tests {
-            let value = run(input).unwrap();
-            assert_eq!(value, expected);
-        }
-    }
-
-    #[test]
-    fn test_locals() {
-        let tests = &[(r#" let x: int = 0; x "#, Value::Int(0)),
-                      (r#"
-                            let x: int = 1;
-                            let y: int = 2;
-                            x + y
-         "#,
-                       Value::Int(3))];
-
-        for &(input, expected) in tests {
-            let value = run(input).unwrap();
-            assert_eq!(value, expected);
-        }
-    }
-
-    #[test]
-    fn test_calls() {
-        let tests = &[(r#"
-                            def add(x: int, y: int): int {
-                                x + y
-                            };
-                            add(1, 2)
-         "#,
-                       Value::Int(3)),
-                      (r#"
-                            def add(x: int, y: int): int {
-                                let z = x + y;
-                                z
-                            };
-                            add(1, 2)
-         "#,
-                       Value::Int(3)),
-                      (r#"
-                            let global: int = 0;
-                            def assign(x: int, y: int) {
-                                global = x + y;
-                            };
-                            assign(1, 2);
-                            global
-         "#,
-                       Value::Int(3))];
-
-        for &(input, expected) in tests {
-            let value = run(input).unwrap();
-            assert_eq!(value, expected);
-        }
-    }
-
-    #[test]
-    fn test_if() {
-        let tests = &[(r#"
-                            def fib(n: int): int {
-                                if n <= 1 {
-                                    n
-                                } else {
-                                    fib(n-2) + fib(n-1)
-                                }
-                            }
-                            fib(8)
-         "#,
-                       Value::Int(21))];
-
-        for &(input, expected) in tests {
-            let value = run(input).unwrap();
-            assert_eq!(value, expected);
-        }
-    }
+    Ok(())
 }
