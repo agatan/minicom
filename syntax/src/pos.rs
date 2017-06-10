3ad08a338b255552645aca92093b650d1c3a44e8
agatan <agatan039@gmail.com>
2017-06-10 12:06:41 +0900

    Add getline function

--- syntax/src/pos.rs
+++ syntax/src/pos.rs
@@ -42,6 +42,62 @@ impl Span {
             end: end,
         }
     }
+
+    pub fn getline<'s>(&self, input: &'s str) -> Option<&'s str> {
+        let start = self.start.absolute.0;
+        if start >= input.len() {
+            return None;
+        }
+        let line_start = input[..start].rfind('\n').map(|x| x + 1).unwrap_or(0);
+        let slice = &input[line_start..];
+        let line_end = slice.find('\n').unwrap_or(slice.len());
+        Some(&slice[..line_end])
+    }
+}
+
+#[test]
+fn test_getline() {
+    let input = "abc\ndef\n";
+    let tests = vec![(Location {
+                          line: Line(1),
+                          column: Column(1),
+                          absolute: Byte(4),
+                      },
+                      Some("def")),
+                     (Location {
+                          line: Line(1),
+                          column: Column(3),
+                          absolute: Byte(6),
+                      },
+                      Some("def")),
+                     (Location {
+                          line: Line(1),
+                          column: Column(4),
+                          absolute: Byte(7),
+                      },
+                      Some("def")),
+                     (Location {
+                          line: Line(0),
+                          column: Column(4),
+                          absolute: Byte(3),
+                      },
+                      Some("abc")),
+                     (Location {
+                          line: Line(0),
+                          column: Column(3),
+                          absolute: Byte(2),
+                      },
+                      Some("abc")),
+                     (Location {
+                          line: Line(0),
+                          column: Column(1),
+                          absolute: Byte(0),
+                      },
+                      Some("abc"))];
+    for (loc, expected) in tests {
+        let span = Span::new(loc, loc);
+        assert_eq!(span.getline(input), expected);
+    }
 }
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
