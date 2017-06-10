bb8fcb99926e98bc65db64e075a082d3801e4441
agatan <agatan039@gmail.com>
2017-06-10 15:16:10 +0900

    Move error module

--- syntax/src/lib.rs
+++ syntax/src/lib.rs
@@ -9,57 +9,17 @@ mod token;
 mod source;
 pub mod pos;
 mod grammar;
+mod error;
 
 pub use source::Source;
 
 use std::convert::From;
 
 use ast::Toplevel;
-use pos::{DUMMY_LOCATION, Location, Spanned};
-use token::{Token, Tokenizer, Error as TokenizeError};
+use pos::Spanned;
+use token::Tokenizer;
 
-quick_error! {
-    #[derive(Debug, PartialEq)]
-    pub enum Error {
-        Token(err: TokenizeError) {
-            description(err.description())
-            display("{}", err)
-            from()
-        }
-        InvalidToken {
-            description("invalid token")
-            display("invalid token")
-        }
-        UnexpectedToken(token: String, expected: Vec<String>) {
-            description("unexpected token")
-            display("unexpected token: {} (expected {:?})", token, expected)
-        }
-        UnexpectedEof(expected: Vec<String>) {
-            description("unexpected EOF")
-            display("unexpected EOF (expected {:?})", expected)
-        }
-        ExtraToken(token: String) {
-            description("extra token")
-            display("extra token: {}", token)
-        }
-    }
-}
-
-impl Error {
-    fn from_lalrpop<'input>(err: lalrpop_util::ParseError<Location, Token<'input>, ()>) -> Spanned<Error> {
-        use lalrpop_util::ParseError::*;
-        match err {
-            InvalidToken { location } => Spanned::new(location, location, Error::InvalidToken),
-            UnrecognizedToken { token: Some((start, token, end)), expected } =>
-                Spanned::new(start, end, Error::UnexpectedToken(token.to_string(), expected)),
-            UnrecognizedToken { token: None, expected } =>
-                Spanned::new(DUMMY_LOCATION, DUMMY_LOCATION, Error::UnexpectedEof(expected)),
-            ExtraToken { token: (start, token, end) } => Spanned::new(start, end , Error::ExtraToken(token.to_string())),
-            User { error: () } => unreachable!(),
-        }
-    }
-}
-
+use error::Error;
 pub struct NodeEnv {
     next_id_: u32,
 }
