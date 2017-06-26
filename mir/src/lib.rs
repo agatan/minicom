0f9e4570a2f9840ed12a6ff61f5a8f81df8526bf
agatan <agatan039@gmail.com>
2017-06-26 23:24:46 +0900

    Define MIR

--- mir/src/lib.rs
+++ mir/src/lib.rs
@@ -1,6 +1,13 @@
+extern crate minicom_basis as basis;
+extern crate minicom_syntax as syntax;
+extern crate minicom_typing as typing;
+
+mod mir;
+
+pub use mir::*;
+
 #[cfg(test)]
 mod tests {
     #[test]
-    fn it_works() {
-    }
+    fn it_works() {}
 }
---
+++ mir/src/mir.rs
@@ -0,0 +1,91 @@
+use std::collections::HashMap;
+
+#[derive(Debug, Clone)]
+pub struct Program {
+    pub decls: HashMap<String, Decl>,
+    pub main: Option<Box<Def>>,
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub enum Type {
+    Int,
+    Float,
+    Bool,
+    Unit,
+    Ref(Box<Type>),
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct Node {
+    pub kind: NodeKind,
+    pub typ: Type,
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub enum NodeKind {
+    Unit,
+    Int(i32),
+    Float(f64),
+    Bool(bool),
+    Ident(Var),
+    Call(String, Vec<Node>),
+    AddInt(Box<Node>, Box<Node>),
+    SubInt(Box<Node>, Box<Node>),
+    MulInt(Box<Node>, Box<Node>),
+    DivInt(Box<Node>, Box<Node>),
+    AddFloat(Box<Node>, Box<Node>),
+    SubFloat(Box<Node>, Box<Node>),
+    MulFloat(Box<Node>, Box<Node>),
+    DivFloat(Box<Node>, Box<Node>),
+    Not(Box<Node>),
+    Eq(Box<Node>, Box<Node>),
+    LE(Box<Node>, Box<Node>),
+    LT(Box<Node>, Box<Node>),
+    Block(Vec<Node>),
+    If(Box<Node>, Box<Node>, Option<Box<Node>>),
+    While(Box<Node>, Box<Node>),
+
+    Let(Box<Let>),
+    AssignGlobal(Var, Box<Node>),
+
+    Ref(Box<Node>),
+    Deref(Box<Node>),
+    Assign(Box<Node>, Box<Node>),
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct Var {
+    pub name: String,
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct Let {
+    pub name: String,
+    pub value: Node,
+}
+
+#[derive(Debug, Clone)]
+pub struct Param {
+    pub name: String,
+    pub typ: Type,
+}
+
+#[derive(Debug, Clone)]
+pub struct Def {
+    pub name: String,
+    pub params: Vec<Param>,
+    pub ret: Type,
+    pub body: Node,
+}
+
+#[derive(Debug, Clone)]
+pub struct Decl {
+    pub kind: DeclKind,
+    pub declare_typ: Type,
+}
+
+#[derive(Debug, Clone)]
+pub enum DeclKind {
+    Def(Box<Def>),
+    Let(Box<Let>),
+}
