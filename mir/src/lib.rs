ae8f919ed75b015a16fa88449daa97329ff15d16
agatan <agatan039@gmail.com>
2017-06-26 23:27:57 +0900

    Define errors and public API stub in mir crate

--- mir/src/lib.rs
+++ mir/src/lib.rs
@@ -2,6 +2,23 @@ extern crate minicom_basis as basis;
 extern crate minicom_syntax as syntax;
 extern crate minicom_typing as typing;
 
+#[macro_use]
+extern crate error_chain;
+
+use basis::errors::Error as BasisError;
+
 mod mir;
 
 pub use mir::*;
+
+pub mod errors {
+    error_chain! {
+        errors { }
+    }
+}
+
+pub type Result<T> = ::std::result::Result<T, BasisError<self::errors::Error>>;
+
+pub fn compile_typed_module(_module: typing::Module) -> Result<Program> {
+    unimplemented!()
+}
