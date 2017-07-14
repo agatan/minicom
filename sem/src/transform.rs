432cbbf28aeddca234c0c2a5c8f0f52f15b0b53c
agatan <agatan039@gmail.com>
2017-07-14 23:54:06 +0900

    Implement type transformation

--- sem/src/transform.rs
+++ sem/src/transform.rs
@@ -1,3 +1,4 @@
+use basis::errors::Error as BasisError;
 use mir;
 
 use Result;
@@ -13,40 +14,60 @@ impl Transform {
         Transform { program: mir::Program::new() }
     }
 
-    fn typ(&mut self, typ: Type) -> mir::Type {
+    fn typ(&mut self, typ: Type) -> Result<mir::Type> {
+        match typ {
+            Type::Unit => Ok(mir::Type::Unit),
+            Type::Int => Ok(mir::Type::Int),
+            Type::Bool => Ok(mir::Type::Bool),
+            Type::Float => Ok(mir::Type::Float),
+            Type::Ref(inner) => {
+                let inner = Box::new(self.typ(*inner)?);
+                Ok(mir::Type::Ref(inner))
+            }
+            Type::Fun(_) => Err(BasisError::from_error(
+                "function type value is not implemented yet",
+            )),
+            Type::Var(inner) => {
+                match *inner.borrow() {
+                    None => Err(BasisError::from_error(
+                        "typing is failed: undetermined type variable",
+                    )),
+                    Some(ref typ) => self.typ(typ.clone()),
+                }
+            }
+        }
+    }
+
+    fn node(&mut self, node: Node) -> Result<mir::Node> {
         unimplemented!()
     }
 
-    fn node(&mut self, node: Node) -> mir::Node {
-        unimplemented!()
-    }
-
-    fn def(&mut self, def: Def) -> mir::Def {
-        let body = self.node(def.body);
+    fn def(&mut self, def: Def) -> Result<mir::Def> {
+        let body = self.node(def.body)?;
         let params = def.params
             .into_iter()
             .map(|p| {
-                let typ = self.typ(p.typ);
-                mir::Param {
+                let typ = self.typ(p.typ)?;
+                Ok(mir::Param {
                     name: p.name,
                     typ: typ,
-                }
+                })
             })
-            .collect();
-        let ret = self.typ(def.ret);
-        mir::Def {
+            .collect::<Result<_>>()?;
+        let ret = self.typ(def.ret)?;
+        Ok(mir::Def {
             name: def.name,
             ret: ret,
             params: params,
             body: body,
-        }
+        })
     }
 
-    fn register_decl(&mut self, name: String, decl: Decl) {
+    fn register_decl(&mut self, name: String, decl: Decl) -> Result<()> {
         match decl.kind {
             DeclKind::Def(def) => {
                 let is_main = def.is_main();
-                let def = self.def(*def);
+                let def = self.def(*def)?;
                 if is_main {
                     self.program.main = Some(Box::new(def));
                 } else {
@@ -60,7 +81,7 @@ impl Transform {
                     value,
                     ..
                 } = l;
-                let node = self.node(value);
+                let node = self.node(value)?;
                 let l = mir::Let {
                     name: let_name,
                     value: node,
@@ -68,17 +89,19 @@ impl Transform {
                 self.program.define(name, mir::Decl::Let(Box::new(l)));
             }
         }
+        Ok(())
     }
 
-    pub fn module(&mut self, module: Module) {
+    pub fn module(&mut self, module: Module) -> Result<()> {
         for (decl_name, decl) in module.decls.into_iter() {
-            self.register_decl(decl_name, decl);
+            self.register_decl(decl_name, decl)?;
         }
+        Ok(())
     }
 }
 
-pub fn typed_ast_to_mir(module: Module) -> mir::Program {
+pub fn typed_ast_to_mir(module: Module) -> Result<mir::Program> {
     let mut transform = Transform::new();
-    transform.module(module);
-    transform.program
+    transform.module(module)?;
+    Ok(transform.program)
 }
